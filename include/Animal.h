659319d6804513351b82b1c4c5329c03732c274b
mateo9516 <mateo.echeverry96@gmail.com>
2025-05-11 09:31:42 -0500

    agrego clase animal

---
+++ include/Animal.h
@@ -0,0 +1,17 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+
+class Animal
+{
+    public:
+        Animal();
+
+    protected:
+
+    private:
+        int numero;
+        char letra;
+};
+
+#endif // ANIMAL_H
---
+++ src/Animal.cpp
@@ -0,0 +1,8 @@
+#include "Animal.h"
+
+Animal::Animal()
+{
+    //ctor
+}
+
+
